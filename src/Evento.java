51577dddc5353881cfdd87b8c693c997b5ac2466
GranCacioRomano <falconetti.grafico@gmail.com>
2025-07-03 16:37:49 +0200

    Creazione interfaccia Partecipante, classe Utente, Classe Evento.

---
+++ src/Evento.java
@@ -0,0 +1,27 @@
+import observer.Partecipante;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Evento {
+    private String nome;
+    private StatoEvento statoEvento;
+    private List<Partecipante> partecipanteList;
+
+    public Evento(String nome, StatoEvento statoEvento, List<Partecipante> partecipanteList) {
+        this.nome = nome;
+        this.statoEvento = statoEvento;
+        this.partecipanteList = new ArrayList<>();
+    }
+
+    public void aggiungiPartecipante(Partecipante partecipante){
+        partecipanteList.add(partecipante);
+
+    }
+
+    public void notificaPartecipante(String messaggio){
+        for(Partecipante partecipante : partecipanteList){
+            partecipante.aggiorna(messaggio);
+        }
+    }
+}
