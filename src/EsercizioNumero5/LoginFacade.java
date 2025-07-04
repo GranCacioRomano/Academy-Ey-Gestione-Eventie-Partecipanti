ed6cab7004f893e1cd78faac610513ddd66b4551
martinaals <martinalaserra2001@gmail.com>
2025-07-04 16:32:22 +0200

    update

--- src/EsercizioNumero5/LoginFacade.java
+++ src/EsercizioNumero5/LoginFacade.java
@@ -1,4 +1,27 @@
 package EsercizioNumero5;
 
 public class LoginFacade {
+
+    public boolean eseguiLogin(String tipo, String utente, String credenziali)
+    {
+        LoginFactory factory;
+
+        switch (tipo.toLowerCase())
+        {
+            case "password":
+                factory = new CreaPasswordLogin();
+                break;
+            case "token":
+                factory = new CreaTokenLogin();
+                break;
+            case "social":
+                factory = new CreaSocialLogin();
+                break;
+            default:
+                return false;
+        }
+
+        LoginStrategy strategia = factory.creaLogin();
+        return strategia.login(utente, credenziali);
+    }
 }
