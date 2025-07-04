9b8f4f768e85dc38966109b8785daf87c349b3b9
GranCacioRomano <falconetti.grafico@gmail.com>
2025-07-04 17:07:02 +0200

    Add ControlloRicetta interface and Ricetta class for recipe management

---
+++ src/EsercizioNumero8/Ricetta.java
@@ -0,0 +1,30 @@
+package EsercizioNumero8;
+
+public class Ricetta {
+    private String nome;
+    private boolean keto;
+    private boolean vegana;
+
+    public Ricetta(String nome, boolean keto, boolean vegana) {
+        this.nome = nome;
+        this.keto = keto;
+        this.vegana = vegana;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean isKeto() {
+        return keto;
+    }
+
+    public boolean isVegana() {
+        return vegana;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
---
+++ src/EsercizioNumero8/Strategy/ControlloRicetta.java
@@ -0,0 +1,9 @@
+package EsercizioNumero8.Strategy;
+
+import EsercizioNumero8.Ricetta;
+
+import java.util.List;
+
+public interface ControlloRicetta {
+    List<Ricetta> controllo(List<Ricetta> ricette);
+}
