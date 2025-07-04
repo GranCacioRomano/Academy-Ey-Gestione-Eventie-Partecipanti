1debec83fd70e8d8441c730495468717865f7bc1
GranCacioRomano <falconetti.grafico@gmail.com>
2025-07-04 15:23:45 +0200

    Initialize Observer and add Disposivo instances in Main class

--- src/EsercizioNumero4/Main.java
+++ src/EsercizioNumero4/Main.java
@@ -1,7 +1,20 @@
 package EsercizioNumero4;
 
+import EsercizioNumero4.observer.*;
+
 public class Main {
     public static void main(String[] args) {
+        Observer o = new Observer();
+
+        Disposivo luce = new Luce("Luce Ingresso");
+        Disposivo allarme = new Allarme("Allarme porta");
+        Disposivo postazione = new Postazione("Postazione Lavoro");
+
+        o.aggiungiDispositivo(luce);
+        o.aggiungiDispositivo(allarme);
+        o.aggiungiDispositivo(postazione);
+
+        o.segnalaEvento("Arrivo a casa!");
 
     }
 }
